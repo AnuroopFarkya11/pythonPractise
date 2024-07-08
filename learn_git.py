21d1fc8a575d17652d72e3270c6de04308a5d1ac
anuroop <anuroopf02@gmail.com>
2024-07-09 01:05:21 +0530

    Learn git command

---
+++ learn_git.py
@@ -0,0 +1,18 @@
+print("HELLO, WE GONNA LEARN GIT COMMANDS IN THIS PROGRAM")
+
+print("Write a command to initialize a repository ")
+res = input()
+
+if(res == 'git init'):
+    print("Success")
+else:
+    print("Wrong answer")
+
+
+print("Write a command to add a file")
+res = input()
+
+if(res == "git add filename"):
+    print("Success")
+else:
+    print("Wrong Answer")
