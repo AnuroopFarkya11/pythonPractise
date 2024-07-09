39c663d07902c2ed3dc6ea212c266bbf9762adb2
anuroop <anuroopf02@gmail.com>
2024-07-10 01:22:57 +0530

    List basics

---
+++ list.py
@@ -0,0 +1,34 @@
+# Declaring a list
+
+myList = ['A','B','C']
+
+# Declare using constructor
+
+myList2 = list(('a','b','c'))
+
+
+
+# Print a list 
+
+print(myList)
+
+# Prove list is ordered 
+
+print(myList[0])
+
+# Prove list is mutable 
+
+myList[0] ='a'
+
+print(myList)
+
+
+# Prove allow duplicates
+
+myList.append('a')
+
+
+print(myList)
+
+
+
