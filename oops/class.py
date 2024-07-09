a64f026ff74b040ba0854510be4cfe1b2648b442
anuroop <anuroopf02@gmail.com>
2024-07-10 01:44:49 +0530

    OOPS

---
+++ oops/class.py
@@ -0,0 +1,11 @@
+class MyClass:
+    def __init__(self):
+        print("My class default constructor initiated")
+    
+    def __init__(self, data):
+        print("My class with data ",data)
+
+
+
+
+obj = MyClass('Ram')
