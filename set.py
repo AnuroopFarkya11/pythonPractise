329b028067e57143941b31ee99e780edc0814ac4
anuroop <anuroopf02@gmail.com>
2024-07-09 09:02:12 +0530

    Set basics

--- set.py
+++ set.py
@@ -4,5 +4,15 @@ mySet = {"a","b","c"}
 mySet2 = set((1,2,3))
 
 # Print a set 
-
 print(mySet) 
+
+# Add element in a set 
+mySet.add("d")
+
+# Length of a set 
+l = len(mySet)
+print("Length of a set ",l)
+
+
+
+
